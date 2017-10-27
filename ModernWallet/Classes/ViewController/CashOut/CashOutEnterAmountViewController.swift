75f3486ea368a433850a14b026da42c40919d3e2
Nacho Nachev <nacho.nachev@primeholding.com>
2017-10-27 17:36:51 +0300

    Implement CashOutPersonalDetailsViewController

--- ModernWallet/Classes/ViewController/CashOut/CashOutEnterAmountViewController.swift
+++ ModernWallet/Classes/ViewController/CashOut/CashOutEnterAmountViewController.swift
@@ -156,15 +156,16 @@ class CashOutEnterAmountViewController: UIViewController {
         }
     }
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "NextStep" {
+            guard let vc = segue.destination as? CashOutPersonalDetailsViewController else {
+                return
+            }
+            vc.cashOutViewModel = cashOutViewModel
+        }
     }
-    */
     
 }
 
---
+++ ModernWallet/Classes/ViewController/CashOut/CashOutPersonalDetailsViewController.swift
@@ -0,0 +1,93 @@
+//
+//  CashOutPersonalDetailsViewController.swift
+//  ModernWallet
+//
+//  Created by Nacho Nachev on 24.10.17.
+//  Copyright © 2017 Lykkex. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import WalletCore
+
+class CashOutPersonalDetailsViewController: UIViewController {
+    
+    @IBOutlet private weak var backgroundHeightConstraint: NSLayoutConstraint!
+    
+    @IBOutlet internal weak var scrollView: UIScrollView!
+    
+    @IBOutlet private weak var subtitleLabel: UILabel!
+    @IBOutlet fileprivate weak var nameTextField: UITextField!
+    @IBOutlet fileprivate weak var reasonTextField: UITextField!
+    @IBOutlet fileprivate weak var notesTextField: UITextField!
+    
+    @IBOutlet fileprivate weak var nextButton: UIButton!
+    
+    var cashOutViewModel: CashOutViewModel!
+    
+    private var disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        backgroundHeightConstraint.constant = Display.height
+        
+        subtitleLabel.text = Localize("cashOut.newDesign.personalDetails")
+        nameTextField.placeholder = Localize("cashOut.newDesign.name")
+        reasonTextField.placeholder = Localize("cashOut.newDesign.transactionReason")
+        notesTextField.placeholder = Localize("cashOut.newDesign.additionalNotes")
+        nextButton.setTitle(Localize("newDesign.next"), for: .normal)
+        
+        let generalViewModel = cashOutViewModel.generalViewModel
+        
+        (nameTextField.rx.textInput <-> generalViewModel.name)
+            .disposed(by: disposeBag)
+        
+        (reasonTextField.rx.textInput <-> generalViewModel.transactionReason)
+            .disposed(by: disposeBag)
+        
+        (notesTextField.rx.textInput <-> generalViewModel.additionalNotes)
+            .disposed(by: disposeBag)
+        
+        let isFormValidDriver = generalViewModel.isValid.asDriver(onErrorJustReturn: false)
+        
+        isFormValidDriver
+            .drive(nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        isFormValidDriver
+            .map { $0 ? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.5970840669) }
+            .drive(nextButton.rx.borderColor)
+            .disposed(by: disposeBag)
+        
+        setupFormUX(disposedBy: disposeBag)
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+
+}
+
+extension CashOutPersonalDetailsViewController: InputForm {
+    
+    var submitButton: UIButton! {
+        return nextButton
+    }
+    
+    var textFields: [UITextField] {
+        return [
+            nameTextField,
+            reasonTextField,
+            notesTextField
+        ]
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return goToTextField(after: textField)
+    }
+    
+}
