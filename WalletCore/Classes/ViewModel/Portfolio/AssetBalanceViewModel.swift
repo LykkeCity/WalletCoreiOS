30410677d3e15bc4eead069010dfb42824028414
Georgi Stanev <georgi.stanev@primeholding.com>
2017-11-15 18:37:28 +0200

    LMW-60 Implement AssetBalanceViewModel

---
+++ WalletCore/Classes/ViewModel/Portfolio/AssetBalanceViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  AssetBalanceViewModel.swift
+//  WalletCore
+//
+//  Created by Georgi Stanev on 15.11.17.
+//  Copyright © 2017 Lykke. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+public class AssetBalanceViewModel {
+    
+    public let assetBalance: Driver<String>
+    public let assetCode: Driver<String>
+    public let assetBalanceInBase: Driver<String>
+    public let baseAssetCode: Driver<String>
+    
+    public init(asset: Observable<Asset>) {
+        assetBalance = asset.mapToCryptoAmount().asDriver(onErrorJustReturn: "")
+        assetBalanceInBase = asset.mapToRealAmount().asDriver(onErrorJustReturn: "")
+        assetCode = asset.mapToCryptoCode().asDriver(onErrorJustReturn: "")
+        baseAssetCode = asset.mapToRealCode().asDriver(onErrorJustReturn: "")
+    }
+}
