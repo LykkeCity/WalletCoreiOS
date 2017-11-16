8c50af1582a4d8c26077a55afd61c42ff2781168
Nacho Nachev <nacho.nachev@primeholding.com>
2017-11-16 17:51:36 +0200

    Links main settings screen in the side menu

--- ModernWallet/Classes/ViewController/Menu/MenuTableViewController.swift
+++ ModernWallet/Classes/ViewController/Menu/MenuTableViewController.swift
@@ -57,8 +57,7 @@ class MenuTableViewController: UITableViewController {
                  viewControllerIdentifier: "commingSoonVC", storyboardName: "Main", color: MenuTableViewController.commingSoonColor),
         MenuItem(title: Localize("menu.newDesign.send"), image: #imageLiteral(resourceName: "SEND"),
                  viewControllerIdentifier: "commingSoonVC", storyboardName: "Main", color: MenuTableViewController.commingSoonColor),
-        MenuItem(title: Localize("menu.newDesign.settings"), image: #imageLiteral(resourceName: "SETTINGS"),
-                 viewControllerIdentifier: "commingSoonVC", storyboardName: "Main", color: MenuTableViewController.commingSoonColor),
+        MenuItem(title: Localize("menu.newDesign.settings"), image: #imageLiteral(resourceName: "SETTINGS"), storyboardName: "Settings"),
         MenuItem(title: Localize("menu.newDesign.transactions"), image: #imageLiteral(resourceName: "TRANSACTIONS"), viewControllerIdentifier: "transactionVC"),
         MenuItem(title: Localize("menu.newDesign.logout"), image: nil, viewControllerIdentifier: nil, color: nil,
                  onSelect: MenuTableViewController.logout)
@@ -232,10 +231,12 @@ class MenuTableViewController: UITableViewController {
         else {
             storyboard = self.storyboard!
         }
-        guard let identifier = menuItem.viewControllerIdentifier else {
-            return nil
+        if let identifier = menuItem.viewControllerIdentifier {
+            return storyboard.instantiateViewController(withIdentifier: identifier)
         }
-        return storyboard.instantiateViewController(withIdentifier: identifier)
+        else {
+            return storyboard.instantiateInitialViewController()
+        }
     }
     
     private static func logout(_ viewController: UIViewController) {
