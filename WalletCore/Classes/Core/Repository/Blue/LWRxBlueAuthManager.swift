dcd083794ad11874ea8bd228da37eaee72f283f9
Georgi Stanev <georgi.stanev@primeholding.com>
2017-11-28 16:43:10 +0200

    Implement Twitter Blue Packet/Repo

---
+++ WalletCore/Classes/Core/Repository/Blue/LWRxBlueAuthManager.swift
@@ -0,0 +1,16 @@
+//
+//  LWRxBlueAuthManager.swift
+//  WalletCore
+//
+//  Created by Georgi Stanev on 28.11.17.
+//  Copyright © 2017 Lykke. All rights reserved.
+//
+
+import Foundation
+
+public class LWRxBlueAuthManager {
+    public static let instance = LWRxBlueAuthManager()
+    private init() {}
+    
+    public lazy var twitterJson = { LWRxAuthManagerTwitterTimeLineJson() }()
+}
