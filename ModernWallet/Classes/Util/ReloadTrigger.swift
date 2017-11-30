40b6d2ef0026493edf0dc13028ca9e466137c73e
Nacho Nachev <nacho.nachev@sbnd.net>
2017-11-30 17:36:40 +0200

    Create global reload trigger

---
+++ ModernWallet/Classes/Util/ReloadTrigger.swift
@@ -0,0 +1,36 @@
+//
+//  ReloadTrigger.swift
+//  ModernMoney
+//
+//  Created by Nacho Nachev on 30.11.17.
+//  Copyright © 2017 Lykkex. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class ReloadTrigger {
+    
+    static let instance = ReloadTrigger()
+    
+    private let trigger: PublishSubject<Void>
+    
+    func trigger(interval: RxTimeInterval) -> Observable<Void> {
+        return Observable.merge([trigger.asObservable(), Observable.interval(interval)])
+    }
+    
+    var triggerWithNow: Observable<Void> {
+        return Observable.merge([trigger.asObservable(), Observable.just(Void())])
+    }
+    
+    func reload() {
+        trigger.onNext(Void())
+    }
+    
+    // MARK: - Private
+    
+    private init() {
+        trigger = PublishSubject()
+    }
+    
+}
