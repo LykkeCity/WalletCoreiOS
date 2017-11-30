366bae9e142d5227677f73d70caf0aa9b8e8a989
Nacho Nachev <nacho.nachev@sbnd.net>
2017-11-30 17:50:00 +0200

    Implement NoConnectionViewController

---
+++ ModernWallet/Classes/ViewController/NoConnectionViewController.swift
@@ -0,0 +1,60 @@
+//
+//  NoConnectionViewController.swift
+//  ModernMoney
+//
+//  Created by Nacho Nachev on 30.11.17.
+//  Copyright © 2017 Lykkex. All rights reserved.
+//
+
+import UIKit
+import WalletCore
+
+class NoConnectionViewController: UIViewController {
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var subtitleLabel: UILabel!
+    @IBOutlet private weak var reloadButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = Localize("noNetwork.newDesign.title")
+        subtitleLabel.text = Localize("noNetwork.newDesign.subtitle")
+        reloadButton.setTitle(Localize("noNetwork.newDesign.reload"), for: .normal)
+
+        let center = NotificationCenter.default
+        center.addObserver(
+            self,
+            selector: #selector(self.showNoInternetIfNeeded(_:)),
+            name: NSNotification.Name(rawValue: kNotificationGDXNetAdapterDidFailRequest),
+            object: nil
+        )
+        center.addObserver(
+            self,
+            selector: #selector(self.hideNoInternetConnection),
+            name: NSNotification.Name(rawValue: kNotificationGDXNetAdapterDidReceiveResponse),
+            object: nil
+        )
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @IBAction func reloadTapped() {
+        reloadButton.isEnabled = false
+        LWAuthManager.instance().requestAPIVersion()
+        ReloadTrigger.instance.reload()
+    }
+
+    @objc private func showNoInternetIfNeeded(_ notification: Notification) {
+        guard let ctx = notification.userInfo?[kNotificationKeyGDXNetContext] as? GDXRESTContext else { return }
+        guard let error = ctx.error as NSError?, error.domain == NSURLErrorDomain else { return }
+        reloadButton.isEnabled = true
+    }
+    
+    @objc private func hideNoInternetConnection() {
+        dismiss(animated: true)
+    }
+
+}
