d5f48bf3a5e18ef11f57c09ed47d32504259cd20
Georgi Stanev <georgi.stanev@primeholding.com>
2017-12-06 16:59:16 +0200

    LEW-255 Reload community screen every 5 seconds

--- WalletCore/Classes/ViewModel/Blue/CommunityViewModel.swift
+++ WalletCore/Classes/ViewModel/Blue/CommunityViewModel.swift
@@ -24,17 +24,28 @@ public class CommunityViewModel {
     
     public let loadingViewModel: LoadingViewModel
     
-    public init(blueAuthManager: LWRxBlueAuthManager = LWRxBlueAuthManager.instance,
-         authManager: LWRxAuthManager = LWRxAuthManager.instance) {
+    public init(
+        triggerRefresh: Observable<Void>,
+        blueAuthManager: LWRxBlueAuthManager = LWRxBlueAuthManager.instance,
+        authManager: LWRxAuthManager = LWRxAuthManager.instance
+    ) {
+        //arrange requests
+        let comunityUsersCountRequest = triggerRefresh
+            .flatMap{ blueAuthManager.getCommunityUsersCount.request() }
+            .shareReplay(1)
         
-        let comunityUsersCountRequest =  blueAuthManager.getCommunityUsersCount.request()
+        let walletRequest = triggerRefresh
+            .flatMap{ authManager.lykkeWallets.request(byAssetId: blueAuthManager.treeCoinIdentifier) }
+            .shareReplay(1)
+        
+        let baseAssetRequest = triggerRefresh
+            .flatMap{ authManager.baseAsset.request() }
+            .shareReplay(1)
         
         communityUsersCount = comunityUsersCountRequest.filterSuccess()
             .map { NumberFormatter.currencyInstance(accuracy: 0).string(from: $0 as NSNumber) }
             .replaceNilWith("")
             .asDriver(onErrorJustReturn: "")
-        
-        let walletRequest = authManager.lykkeWallets.request(byAssetId: blueAuthManager.treeCoinIdentifier)
         
         let wallet = walletRequest.filterSuccess().filterNil().shareReplay(1)
         
@@ -48,8 +59,6 @@ public class CommunityViewModel {
             .replaceNilWith("")
             .asDriver(onErrorJustReturn: "")
         
-        let baseAssetRequest = authManager.baseAsset.request()
-        
         amountInBaseAsset = Observable.combineLatest(wallet, baseAssetRequest.filterSuccess())
             .map { data in
                 let (wallet, baseAsset) = data
@@ -59,7 +68,11 @@ public class CommunityViewModel {
         
         zeroBalance = wallet.map{$0.balance == 0.0}.asDriver(onErrorJustReturn: true)
         
-        loadingViewModel = LoadingViewModel([comunityUsersCountRequest.isLoading(), walletRequest.isLoading(), baseAssetRequest.isLoading()])
+        //show loading indicator only for initial loading
+        loadingViewModel = LoadingViewModel([
+            comunityUsersCountRequest.isLoading().take(2),
+            walletRequest.isLoading().take(2),
+            baseAssetRequest.isLoading().take(2)
+        ])
     }
-    
 }
