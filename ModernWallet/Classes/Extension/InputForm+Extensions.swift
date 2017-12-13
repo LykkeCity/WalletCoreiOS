8ddc277a6aa175724805f8f7ec4aafd0ee3d50e0
Nacho Nachev <nacho.nachev@sbnd.net>
2017-12-13 16:09:37 +0200

    Set InputForm’s theme colors

---
+++ ModernWallet/Classes/Extension/InputForm+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  InputForm+Extensions.swift
+//  ModernMoney
+//
+//  Created by Nacho Nachev on 13.12.17.
+//  Copyright © 2017 Lykkex. All rights reserved.
+//
+
+import WalletCore
+
+extension InputForm {
+    
+    var accessoryButtonBackgroundColor: UIColor { return #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) }
+    
+    var accessoryButtonTextColor: UIColor { return #colorLiteral(red: 0, green: 0.431372549, blue: 0.3411764706, alpha: 1) }
+    
+}
