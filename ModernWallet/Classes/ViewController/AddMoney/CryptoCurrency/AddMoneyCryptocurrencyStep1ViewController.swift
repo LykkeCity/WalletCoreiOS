15bd223000e1763e7f839a9bba72c7025f65a6a8
Ivan Stefanovic <ibatica@gmail.com>
2017-12-21 13:33:39 +0100

    LMW-120 Receive Crypto Currency: Listing

--- ModernWallet/Classes/ViewController/AddMoney/CryptoCurrency/AddMoneyCryptocurrencyStep1ViewController.swift
+++ ModernWallet/Classes/ViewController/AddMoney/CryptoCurrency/AddMoneyCryptocurrencyStep1ViewController.swift
@@ -25,6 +25,9 @@ class AddMoneyCryptocurrencyStep1ViewController: UIViewController {
         currenciesTableView.backgroundColor = UIColor.clear
 
         currenciesTableView.register(UINib(nibName: "AddMoneyCryptoCurrencyTableViewCell", bundle: nil), forCellReuseIdentifier: "AddMoneyCryptoCurrencyTableViewCell")
+//        let allCryptoAssets = LWRxAuthManager.instance.allAssets.request()
+//        .filterSuccess()
+//            .map{$0.filter{$0.blockchainDeposit}}
         
         LWRxAuthManager.instance.lykkeWallets.request()
             .filterSuccess()
@@ -32,9 +35,13 @@ class AddMoneyCryptocurrencyStep1ViewController: UIViewController {
                             return ($0 as! LWSpotWallet).asset.blockchainDeposit
                 }.map({ (wallet) -> Variable<LWAddMoneyCryptoCurrencyModel> in
                     let w: LWSpotWallet = wallet as! LWSpotWallet
-                    return Variable(LWAddMoneyCryptoCurrencyModel(name:w.name,
-                                                                address:w.asset.blockchainDepositAddress,
-                                                                imageUrl:URL(string: w.asset.iconUrlString)))
+                    let model = LWAddMoneyCryptoCurrencyModel(name:w.name,
+                                                              address:w.asset.blockchainDepositAddress,
+                                                              imageUrl:w.asset.iconUrl)
+//                    self.findImageUrl(identity: w.identity,allCryptoAssets: allCryptoAssets).subscribe(onNext: { url in
+//                        model.imgUrl = URL(string: (url[0]?.absoluteString)!)
+//                        }).addDisposableTo(self.disposeBag)
+                    return Variable(model)
                 })
                 
             }.bind(to: currenciesTableView.rx.items(cellIdentifier: "AddMoneyCryptoCurrencyTableViewCell",
@@ -54,6 +61,10 @@ class AddMoneyCryptocurrencyStep1ViewController: UIViewController {
         
     }
 
+    func findImageUrl(identity:String?, allCryptoAssets:Observable<[LWAssetModel]>) -> Observable<[URL?]>{
+        let url = allCryptoAssets.map{$0.filter{$0.identity == identity}.map{$0.iconUrl}}
+        return url
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
