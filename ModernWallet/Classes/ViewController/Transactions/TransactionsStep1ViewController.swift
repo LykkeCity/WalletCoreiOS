5c99e1d43067d8059c48dd08afbc3d54fc16dcfb
Ivan Stefanovic <ibatica@gmail.com>
2018-02-09 13:36:09 +0100

    LMW-359 The search in the `Transactions` screen pops the AirDrop sheet

--- ModernWallet/Classes/ViewController/Transactions/TransactionsStep1ViewController.swift
+++ ModernWallet/Classes/ViewController/Transactions/TransactionsStep1ViewController.swift
@@ -143,7 +143,9 @@ fileprivate extension TransactionsViewModel {
             
             sortBy.asDriver()
                 .map{ $0.asImage() }
-                .drive(vc.sortIcon.rx.image)
+                .drive(vc.sortIcon.rx.image),
+            
+            errors.drive(vc.rx.error)
         ]
     }
 }
