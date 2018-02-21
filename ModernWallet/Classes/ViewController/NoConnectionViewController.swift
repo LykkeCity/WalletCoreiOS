0ccea618e69a12d183787f36be290071bc526b63
Lyubomir Marinov <lyubomir.tsekov@primeholding.com>
2018-02-21 11:33:51 +0200

    LMW-268 - remove "Reload" button from the No Network screen

--- ModernWallet/Classes/ViewController/NoConnectionViewController.swift
+++ ModernWallet/Classes/ViewController/NoConnectionViewController.swift
@@ -23,21 +23,6 @@ class NoConnectionViewController: UIViewController {
         
         titleLabel.text = Localize("noNetwork.newDesign.title")
         subtitleLabel.text = Localize("noNetwork.newDesign.subtitle")
-        reloadButton.setTitle(Localize("noNetwork.newDesign.reload"), for: .normal)
-
-        let center = NotificationCenter.default
-        center.addObserver(
-            self,
-            selector: #selector(self.showNoInternetIfNeeded(_:)),
-            name: NSNotification.Name(rawValue: kNotificationGDXNetAdapterDidFailRequest),
-            object: nil
-        )
-        center.addObserver(
-            self,
-            selector: #selector(self.hideNoInternetConnection),
-            name: NSNotification.Name(rawValue: kNotificationGDXNetAdapterDidReceiveResponse),
-            object: nil
-        )
         
         ReachabilityService.instance
             .reachabilityStatus
@@ -47,26 +32,6 @@ class NoConnectionViewController: UIViewController {
                 self?.dismiss(animated: true)
             })
             .disposed(by: disposeBag)
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
-
-    @IBAction func reloadTapped() {
-        reloadButton.isEnabled = false
-        LWAuthManager.instance().requestAPIVersion()
-        ReloadTrigger.instance.reload()
-    }
-
-    @objc private func showNoInternetIfNeeded(_ notification: Notification) {
-        guard let ctx = notification.userInfo?[kNotificationKeyGDXNetContext] as? GDXRESTContext else { return }
-        guard let error = ctx.error as NSError?, error.domain == NSURLErrorDomain else { return }
-        reloadButton.isEnabled = true
-    }
-    
-    @objc private func hideNoInternetConnection() {
-        dismiss(animated: true)
     }
 
 }
