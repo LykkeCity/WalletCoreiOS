019bb05c86ba21ddadb10fea1a5ad6fbb5bc8a04
Vladimir Dimov <vladimir.dimov@primeholding.com>
2018-07-10 09:45:12 +0300

    The string messages have been replaced with localize variables

--- WalletCore/Classes/Core/Service/Validator/CreditCardInputValidator.swift
+++ WalletCore/Classes/Core/Service/Validator/CreditCardInputValidator.swift
@@ -12,35 +12,35 @@ public class CreditCardInputValidator {
     func validate(input: LWPacketGetPaymentUrlParams) -> ApiResult<LWPacketGetPaymentUrl>{
         
         guard let amountValue = input.amount.decimalValue, amountValue > 0 else {
-            return .error(withData: ["Message": "Please fill in the Amount field."])
+            return .error(withData: ["Message": Localize("addMoney.newDesign.emptyAmountField")])
         }
         
         if  input.firstName.isEmpty {
-            return .error(withData: ["Message": "Please fill in the FirstName field.", "Field": "FirstName"])
+            return .error(withData: ["Message": Localize("addMoney.newDesign.emptyFirstNameField"), "Field": "FirstName"])
         }
         
         if  input.lastName.isEmpty {
-            return .error(withData: ["Message": "Please fill in the LastName field.", "Field": "LastName"])
+            return .error(withData: ["Message": Localize("addMoney.newDesign.emptyLastNameField"), "Field": "LastName"])
         }
         
         if  input.address.isEmpty {
-            return .error(withData: ["Message": "Please fill in the Address field.", "Field": "Address"])
+            return .error(withData: ["Message": Localize("addMoney.newDesign.emptyAddressField"), "Field": "Address"])
         }
         
         if  input.city.isEmpty {
-            return .error(withData: ["Message": "Please fill in the City field.", "Field": "City"])
+            return .error(withData: ["Message": Localize("addMoney.newDesign.emptyCityField"), "Field": "City"])
         }
         
         if  input.zip.isEmpty {
-            return .error(withData: ["Message": "Please fill in the Zip field.", "Field": "Zip"])
+            return .error(withData: ["Message": Localize("addMoney.newDesign.emptyZipField"), "Field": "Zip"])
         }
         
         if input.country.isEmpty {
-            return .error(withData: ["Message": "Please fill in the Country field.", "Field": "Country"])
+            return .error(withData: ["Message": Localize("addMoney.newDesign.emptyCountryField"), "Field": "Country"])
         }
         
         if input.phone.isEmpty {
-            return .error(withData: ["Message": "Please fill in the Code and Phone fields.", "Field": "Phone"])
+            return .error(withData: ["Message": Localize("addMoney.newDesign.emptyCodeAndPhoneField"), "Field": "Phone"])
         }
                 
         return ApiResult.success(withData: LWPacketGetPaymentUrl())
