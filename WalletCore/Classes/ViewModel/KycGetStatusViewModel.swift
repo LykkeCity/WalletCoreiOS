8fd3247092659077d019c38c34d774b850da18b1
Lyubomir Marinov <lyubomir.tsekov@primeholding.com>
2018-08-15 18:07:09 +0300

    LMW-341 - move the logic from the View Controller to the View Model

--- WalletCore/Classes/ViewModel/KycGetStatusViewModel.swift
+++ WalletCore/Classes/ViewModel/KycGetStatusViewModel.swift
@@ -11,6 +11,9 @@ import RxSwift
 
 public class KycGetStatusViewModel {
     
+    /// UserDefaults key for `KYC approval screen is shown` boolean
+    public static let kycSaveKey: String = "KycApprovalScreenShownToUser"
+    
     /// Loading indicator
     public let loadingViewModel: LoadingViewModel
     
@@ -23,7 +26,18 @@ public class KycGetStatusViewModel {
             .request()
             .shareReplay(1)
         
-        self.kycStatusОк = kycStatusGet
+        self.kycStatusОк = UserDefaults.standard.rx
+            .observe(String.self, KycGetStatusViewModel.kycSaveKey)
+            .map { storedValue in
+                guard let storedValue = storedValue else {
+                    // No value yet added to UserDefaults (fresh install)
+                    return true
+                }
+                
+                return storedValue != LWKeychainManager.instance().login
+            }
+            .filter { $0 }
+            .flatMapLatest { _ in kycStatusGet }
             .filterSuccess()
             .filter { $0.status.lowercased() == "ok" }
             .map{ _ in () }
