5b68127bd008e705ab439b2ec55a4ae1cbf553dd
Vladimir Dimov <vladimir.dimov@primeholding.com>
2018-11-13 09:36:50 +0200

    Fix the PinGetViewModel

--- WalletCore/Classes/ViewModel/PinGetViewModel.swift
+++ WalletCore/Classes/ViewModel/PinGetViewModel.swift
@@ -12,30 +12,29 @@ import RxCocoa
 
 open class PinGetViewModel{
     
-    public let pin = Variable<String>("")
-    public let loading: Observable<Bool>
-    public let result: Driver<ApiResult<LWPacketPinSecurityGet>>
+    //IN
+    public let pinTrigger = PublishSubject<String>()
     
-    public init(submit: Observable<Void>, authManager: LWRxAuthManager = LWRxAuthManager.instance)
+    //OUT
+    public let result: Observable<LWPacketPinSecurityGet>
+    
+    public let error: Observable<[AnyHashable: Any]>
+    
+    public let loadingViewModel: LoadingViewModel
+    
+    public init(authManager: LWRxAuthManager = LWRxAuthManager.instance)
     {
-        result = submit
+        let request = pinTrigger
             .throttle(1, scheduler: MainScheduler.instance)
-            .mapToPack(pin:pin, authManager: authManager)
-            .asDriver(onErrorJustReturn: ApiResult.error(withData: [:]))
+            .flatMapLatest{ pin in
+                authManager.pinget.request(withParams: pin)
+            }
+            .shareReplay(1)
         
-        loading = self.result.asObservable().isLoading()
-    }
-}
-
-fileprivate extension ObservableType where Self.E == Void {
-    func mapToPack(
-        pin: Variable<String>,
-        authManager: LWRxAuthManager
-    ) -> Observable<ApiResult<LWPacketPinSecurityGet>> {
+        result = request.filterSuccess()
         
-        return flatMapLatest{authData in
-            authManager.pinget.request(withParams: pin.value)
-        }
-        .shareReplay(1)
+        error = request.filterError()
+        
+        loadingViewModel = LoadingViewModel([request.asObservable().isLoading()])
     }
 }
